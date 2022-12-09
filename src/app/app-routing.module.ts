a821ac68de76c14452e90fd137d77ea67e20065d
Josephshitandi <jsphshtnd@gmail.com>
2022-12-09 07:56:11 +0300

    Added login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [];
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
+import { NgModule } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
+
 
 import { AppModule } from './app/app.module';
+// Import {NgbModule} from ‘@ng-bootstrap/ng-bootstrap’; 
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
